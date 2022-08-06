42137ba6bd15be2fb7b8aacc34113fae269e5788
Shar1q-codes <shariqhussain691@gmail.com>
2022-08-06 23:56:02 +0530

    done

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const dataRouter = require('./Routes/Data')
+const cors = require('cors')
 
 // app.get('/home', function(req,res){
 //     res.send('Home')
 // })
 
+app.use(cors())
 app.use('/api',dataRouter)
 
 app.listen(process.env.PORT||8080, function(){
     console.log("Working");
 })
-//??
